7cd24eb3a141b171b04ca07a535d2dcc7e896bf4
BartoszTonia <54271677+BartoszTonia@users.noreply.github.com>
2021-04-11 07:39:46 +0200

    update show.py description

--- show.py
+++ show.py
@@ -3,6 +3,13 @@ import pandas as pd
 pd.set_option('display.max_columns', 15)
 pd.set_option('display.width', 1600)
 
+# This file is just prepared to take a look at shape of data
+# while scraper is working
+
 df = pd.read_csv('lib/temp.csv')
 print(df)
 print(df.describe().round(2))
+
+# Run ready file with Jupyter Notebook
+# https://github.com/BartoszTonia/otodom_notebook
+
